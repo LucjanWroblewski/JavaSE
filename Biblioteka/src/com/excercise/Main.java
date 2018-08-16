510c0b9d0b5ea6e9f60b2699d8d1f82f601c213a
Lucjan Wroblewski <39348086+LucjanWroblewski@users.noreply.github.com>
2018-08-16 11:20:02 +0200

    Add files via upload

---
+++ Biblioteka/src/com/excercise/Main.java
@@ -0,0 +1,20 @@
+package com.excercise;
+
+public class Main {
+
+    public static void main(String[] args) {
+	final String appName = "Biblioteka v.01";
+	String title = " W pustyni i w Puszczy";
+	String author = " Henryk Sienkiewicz";
+	int releaseDate = 2010;
+	int pages = 296;
+	String publisher = "Greg";
+	String isbn = "939924003101";
+
+        System.out.println("Ksiazki dostepne w bibliotece");
+        System.out.println(title + " " + author  );
+        System.out.println(releaseDate + " " + pages);
+        System.out.println(publisher + " " + isbn);
+
+    }
+}
